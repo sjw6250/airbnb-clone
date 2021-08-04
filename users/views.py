db1f7cda328b502fc3678f0aea48295bb2f9db9c
jwsung <jwsung@gmarineservice.com>
2021-08-05 07:39:11 +0900

    15.sign up without modelforms

--- users/views.py
+++ users/views.py
@@ -1,7 +1,10 @@
+from django.forms.forms import Form
+from django.views.generic import FormView
 from django.views import View
 from django.shortcuts import render, redirect, reverse
 from django.contrib.auth import authenticate, login, logout
 from . import forms
+from django.urls import reverse_lazy
 
 
 class LoginView(View):
@@ -24,3 +27,19 @@ class LoginView(View):
 def log_out(request):
     logout(request)
     return redirect(reverse("core:home"))
+
+
+class SignUpView(FormView):
+    template_name = "users/signup.html"
+    form_class = forms.SignUpForm
+    success_url = reverse_lazy("core:home")
+    initial = {"first_name": "Nicoas", "last_name": "Serr", "email": "itn@las.com"}
+
+    def form_valid(self, form):
+        form.save()
+        email = form.cleaned_data.get("email")
+        password = form.cleaned_data.get("password")
+        user = authenticate(self.request, username=email, password=password)
+        if user is not None:
+            login(self.request, user)
+        return super().form_valid(form)
